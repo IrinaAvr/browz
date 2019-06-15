3b5bd29254c41faeaf38f627604182986bf081ca
unknown <irinaavrutin@gmail.com>
2019-06-15 23:32:40 +0300

    Updates

---
+++ precache-manifest.182c3ef572565439c9ea4989327ea6b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3161327c4608d0df6dfd144564bac8c3",
+    "url": "/browz/index.html"
+  },
+  {
+    "revision": "b42484cada40a7dbc574",
+    "url": "/browz/static/css/main.9ecdc80c.chunk.css"
+  },
+  {
+    "revision": "9106383d4d3cbd8a53ab",
+    "url": "/browz/static/js/2.67841258.chunk.js"
+  },
+  {
+    "revision": "b42484cada40a7dbc574",
+    "url": "/browz/static/js/main.2e322089.chunk.js"
+  },
+  {
+    "revision": "9c835628483e5fbeb5a1",
+    "url": "/browz/static/js/runtime~main.97766991.js"
+  }
+]);
