c10a681105b539e3f235228c0845593e8651e266
IrinaAvr <51863974+IrinaAvr@users.noreply.github.com>
2019-06-16 22:13:14 +0300

    Add files via upload

---
+++ ItemsList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+//import Item from './item.js';
+          
+class ItemsList extends React.Component {
+	constructor(props) {
+		super(props);
+		//this.handleClick = this.handleClick.bind(this);
+		//alert(this.props.selected);
+		this.state={selected:this.props.selected}
+    } 
+
+
+    handleClick = (item, e) =>{
+	
+		this.setState({selected: item});
+		this.props.onClickMethod(item,e);
+		//alert("ItemList Click " + item );
+		//console.log(item);
+    }
+	
+	styleSwitch = (item, e) =>{
+		//alert(item);
+		//alert(this.props.selected);
+		if (item === this.props.selected)
+			return "selItem";
+		else
+			return "liItem";
+	}
+	componentWillReceiveProps(nextProps){
+		console.log(nextProps.selected);
+		this.setState({selected: nextProps.selected});
+		
+		
+	}
+
+    render(){
+		return(
+		<div className = "DivList">         
+			<ul>
+			  { this.props.list.map(function(item) {
+				return <li className= {this.state.selected === item ? 'selItem': 'liItem' } key={item} name={item} onClick={this.handleClick.bind(this,item)}>{item}</li>}, this)
+			  }
+			</ul>
+        </div>);
+    }
+}
+export default ItemsList;
